bfadfd49a394f2a697ac2e350766d8a9f4ed3c2b
Zastashkov <zast2020@mail.ru>
2023-06-03 19:36:07 +0300

    Exercise 1.20 detab is done

---
+++ 1_Getting-started/exercises/exercise-1.20_detab/1.20_detab.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+#define MAXLENGTH 1000
+#define TABSIZE 8
+
+void detab(char s[], int len);
+void copy(char from[], char to[]);
+int get_line(char s[], int lim);
+
+int main() {
+    char line[MAXLENGTH];
+    int len;
+    do {
+        len = get_line(line, MAXLENGTH);
+        detab(line, len);
+    }
+    while (line[len - 1] != EOF);
+    return 0;
+}
+
+void copy(char from[], char to[]) {
+    for (int i = 0; (from[i] = to[i]) != '\0'; ++i);
+}
+
+/* get_line: read a line into s, return length */
+int get_line(char s[], int lim) {
+    int c, i;
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i++] = c;
+    }
+    else if (c == EOF) {
+        s[i++] = c;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+void detab(char s[], int len) {
+    int nonTabCount = 0;
+    for (int i = 0; i < len; ++i) {
+        if (s[i] == '\t') {
+            while (TABSIZE - nonTabCount > 0) {
+                putchar(' ');
+                nonTabCount++;
+            }
+            nonTabCount = 0;
+        } else {
+            putchar(s[i]);
+            nonTabCount++;
+            if (nonTabCount >= TABSIZE)
+                nonTabCount = 0;
+        }
+    }
+}
