0c89404a91118354ba764d258377a951719e3736
Zastashkov <zast2020@mail.ru>
2023-06-13 15:44:18 +0300

    Exercise 2-6 is done. Decompose bit expression into variables and comment each one

--- 2_Types-Operators-Expressions/exercises/exercise-2.6_setbits/2.6_setbits.c
+++ 2_Types-Operators-Expressions/exercises/exercise-2.6_setbits/2.6_setbits.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 
-/**
- *
- * Exercise 2.6 -  Write a function setbits(x,p,n,y) that returns x with the
- * n bits that begin at position p set to the rightmost n bits of y,leaving
- * the other bits unchanged.
- **/
-
-#include <stdio.h>
-
 unsigned setbits(unsigned x, int p, int n, unsigned y);
 
-int main(void) { 
-    printf("%u\n", setbits((unsigned) 255, 3, 4, (unsigned) 0)); 
-    printf("%u", setbits((unsigned) 12, 3, 2, (unsigned) 57)); 
+int main() {
+  printf("%d\n", setbits(255, 3, 4, 0));
+  printf("%d\n", setbits(12, 3, 2, 57));
+  return 0;
 }
 
 unsigned setbits(unsigned x, int p, int n, unsigned y) {
-    unsigned RIGHT_N_1 = ~(~0 << n);  // x = 255; n = 4; p = 3; value = 1111 1111 -> 1111 0000 -> 0000 1111  |||  x = 12; p = 3; n = 2; 1111 1111 -> 1111 1100 -> 0000 0011
-    unsigned BITS_TO_MOVE_FOR_MASK = p + 1 - n; // x = 255; n = 4; p = 3; value = 3 + 1 - 4 = 0  ||| x = 12, p = 3; n = 2,  value = 3 + 1 - 2 = 2; 0000 0011 << 2 -> 0000 1100
-    return x & ~(RIGHT_N_1 << BITS_TO_MOVE_FOR_MASK) | (y & (RIGHT_N_1 << BITS_TO_MOVE_FOR_MASK));
+  unsigned RIGHT_N_1 = ~(~0 << n);  // Get rightmost ones equal to the n bits we need to replace
+  unsigned BITS_TO_MOVE_FOR_MASK = p + 1 - n;  // This var is needed to shift previous value (RIGHT_N_1) so that it matches the location of the bits we need to replace in x 
+  unsigned MASK = RIGHT_N_1 << BITS_TO_MOVE_FOR_MASK;  // This mask in 15 line is used to ing needed bits in x to 0 and ing everything except these bits to 0 in y. So bits we need in mask are 1
+  return x & ~MASK | (y & MASK);
 }
