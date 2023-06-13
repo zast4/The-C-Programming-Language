e942338d8f006b2429033b00c17c30d117892340
Zastashkov <zast2020@mail.ru>
2023-06-13 15:06:45 +0300

    exercise 2.6 is done

--- 2_Types-Operators-Expressions/exercises/exercise-2.6_setbits/2.6_setbits.c
+++ 2_Types-Operators-Expressions/exercises/exercise-2.6_setbits/2.6_setbits.c
@@ -11,8 +11,13 @@
 
 unsigned setbits(unsigned x, int p, int n, unsigned y);
 
-int main(void) { printf("%u", setbits((unsigned) 12, 3, 2, (unsigned) 57)); }
+int main(void) { 
+    printf("%u\n", setbits((unsigned) 255, 3, 4, (unsigned) 0)); 
+    printf("%u", setbits((unsigned) 12, 3, 2, (unsigned) 57)); 
+}
 
 unsigned setbits(unsigned x, int p, int n, unsigned y) {
-    return x & ~(~(~0 << n) << (p + 1 - n)) | (y & (~(~0 << n)) << (p + 1 - n));
+    unsigned RIGHT_N_1 = ~(~0 << n);  // x = 255; n = 4; p = 3; value = 1111 1111 -> 1111 0000 -> 0000 1111  |||  x = 12; p = 3; n = 2; 1111 1111 -> 1111 1100 -> 0000 0011
+    unsigned BITS_TO_MOVE_FOR_MASK = p + 1 - n; // x = 255; n = 4; p = 3; value = 3 + 1 - 4 = 0  ||| x = 12, p = 3; n = 2,  value = 3 + 1 - 2 = 2; 0000 0011 << 2 -> 0000 1100
+    return x & ~(RIGHT_N_1 << BITS_TO_MOVE_FOR_MASK) | (y & (RIGHT_N_1 << BITS_TO_MOVE_FOR_MASK));
 }
