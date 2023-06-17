cf5277c095988d4e6ddf2f4cddca085737d68db4
Daniel Zastashkov <zast2020@mail.ru>
2023-06-17 15:20:08 +0300

    exercise 2.9 is done

--- 2_Types-Operators-Expressions/exercises/exercise-2.9_bitcount-faster-version/2.9_bitcount-faster-version.c
+++ 2_Types-Operators-Expressions/exercises/exercise-2.9_bitcount-faster-version/2.9_bitcount-faster-version.c
@@ -9,13 +9,13 @@
 /* int bitcount: count 1 bits in x */
 int bitcount(unsigned x);
 
-int main() {
-  printf("%u ", (unsigned)rightrot((unsigned)1, (int)2));
-  
+int main() { 
+  printf("%d", bitcount(0));
   return 0;
 }
 
 int bitcount(unsigned x) {
   int i;
-  for (i = 0; )
+  for (i = 0; x != 0; x &= (x - 1), ++i);
+  return i;
 }
