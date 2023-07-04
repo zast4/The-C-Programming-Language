303513d5b65e7d761892c2ec26aac86f88a94bf4
Daniel Zastashkov <zast2020@mail.ru>
2023-07-04 09:40:58 +0300

    exercise 4-2 atof with scientific notation

---
+++ 4_Functions-and-Program-Structure/exercises/exercise-4.2_atof-with-scientific-notation/4.2_atof-with-scientific-notation.c
@@ -0,0 +1,65 @@
+#include <ctype.h>
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int get_line(char s[], int lim);
+double atof(char s[]);
+int power(int base, int exp);
+
+int main() {
+    char str[MAXLINE];
+    double num;
+    get_line(str, MAXLINE);
+    num = atof(str);
+    printf("%.10f", num);
+    return 0;
+}
+
+double atof(char s[]) {
+    int i, sign, esign, exp;
+    double val, pow = 1.0;
+    for (i = 0; isspace(s[i]); ++i)
+        ;
+    sign = (sign == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
+        ++i;
+    for (val = 0.0; isdigit(s[i]); ++i)
+        val = 10.0 * val + (s[i] - '0');
+    if (s[i] == '.')
+        ++i;
+    for (pow = 1.0; isdigit(s[i]); ++i) {
+        val = 10.0 * val + (s[i] - '0');
+        pow *= 10.0;
+    }
+    if (s[i] == 'e' || s[i] == 'E') 
+        ++i;
+    if (s[i] == '-' || s[i] == '+') {
+        esign = s[i];
+        ++i;
+    }
+    for (exp = 0; isdigit(s[i]); ++i)
+        exp = 10.0 * exp + (s[i] - '0');
+    
+    if (esign == '-')
+        return sign * (val / pow) / power(10, exp);
+    else
+        return sign * (val / pow) * power(10, exp);
+}
+
+int get_line(char s[], int lim) {
+    int i, c;
+    while (--lim > 0 && (c = getchar()) != EOF && c != '\0')
+        s[i++] = c;
+    if (c == '\n')
+        s[i++] = '\n';
+    s[i] = '\0';
+    return i;
+}
+
+int power(int base, int exp) {
+    int power = 1;
+    while (exp-- > 0)
+        power *= base;
+    return power;
+}
