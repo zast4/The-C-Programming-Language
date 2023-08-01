f651d56bbb9ff5e30d257bc374c45997de9af845
Daniel Zastashkov <zast2020@mail.ru>
2023-08-01 11:46:32 +0300

    exercises 5-11 - 5-13 entab detab tail with cmd line args

---
+++ 5_Pointers-and-Arrays/exercises/exercise-5.13_tail/5.13_tail.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DEFLINES 10  // default # of lines to print
+#define LINES 100
+#define MAXLEN 100
+
+void error(char *);
+
+int main(int argc, char *argv[]) {
+    char *p;
+    char *buf;  // pointer to the large buffer
+    char *bufend;  // end of the large buffer
+    size_t bufsize = MAXLEN;
+
+    char* line = malloc(MAXLEN * sizeof(char));
+    char *lineptr[LINES];  // pointer to lines read
+
+    int first, i, last, len, n, nlines;
+
+    if (argc == 1)
+        n = DEFLINES;
+    else if (argc == 2 && (*++argv)[0] == '-')
+        n = atoi(argv[0] + 1);
+    else
+        error("Usage: tail [-n]");
+
+    if (n < 1 || n > LINES)
+        n = LINES;
+    
+    for (i = 0; i < LINES; i++)
+        lineptr[i] = NULL;
+
+    if ((p = buf = malloc(LINES * bufsize)) == NULL)
+        error("tail: cannot allocate buf");
+    bufend = buf + LINES + MAXLEN;
+
+    last = 0;
+    nlines = 0;
+
+    while ((len = getline(&line, &bufsize, stdin)) > 0) {
+        if (p + len + 1 >= bufend)
+            p = buf;
+        lineptr[last] = p;
+
+        strcpy(lineptr[last], line);
+        if (++last >= LINES)
+            last = 0;
+
+        p += len + 1;
+        nlines++;
+    }
+
+    if (n > nlines)
+        n = nlines;
+
+    first = last - n;
+
+    if (first < 0)
+        first += LINES;
+
+    for (i = first; n-- > 0; i = (i + 1) % LINES)
+        printf("%s", lineptr[i]);
+
+    return 0;
+}
+
+void error(char *s) {
+    printf("%s\n", s);
+    exit(1);
+}
