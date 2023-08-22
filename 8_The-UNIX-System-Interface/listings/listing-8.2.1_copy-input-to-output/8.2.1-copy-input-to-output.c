4a366fcbc6fcf7869d6723ebeef03584208d4b44
Daniel Zastashkov <zast2020@mail.ru>
2023-08-22 12:02:23 +0300

    exercise 8-1 rewrite cat with write, read functions

---
+++ 8_The-UNIX-System-Interface/listings/listing-8.2.1_copy-input-to-output/8.2.1-copy-input-to-output.c
@@ -0,0 +1,12 @@
+#include "syscalls.h"
+
+#define BUFSIZ 1024
+
+int main() {    /* copy input to output */
+    char buf[BUFSIZ];
+    int n;
+
+    while ((n = read(0, buf, BUFSIZ)) > 0)
+        write(1, buf, n);
+    return 0;
+}
